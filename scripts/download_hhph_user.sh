89379b4fe8ac2a5a45278b9c7dbf4209a6a2d2f7
cosmicBboy <niels.bantilan@gmail.com>
2016-05-09 12:27:56 -0400

    added dates to the download hhph user script

--- scripts/download_hhph_user.sh
+++ scripts/download_hhph_user.sh
@@ -12,12 +12,18 @@ USERNAMES='hhph.breaker1@gmail.com
            hhph.breaker9@gmail.com
            hhph.breaker10@gmail.com'
 
-DATES='2014-11-14
+DATES='2014-10-24
+       2014-11-14
+       2014-11-21
+       2014-12-12
+       2014-12-19
        2015-01-16
        2015-01-23
        2015-02-06
        2015-02-13
-       2015-02-27'
+       2015-02-27
+       2015-03-13
+       2015-03-20'
 
 for u in $USERNAMES; do
     mkdir -p ../data/$u
